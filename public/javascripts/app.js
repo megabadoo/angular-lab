3f518ceb536bfad92c4895db8304169957a62145
Ubuntu <arnell.megan@yahoo.com>
2016-03-22 06:01:44 +0000

    all files

---
+++ public/javascripts/app.js
@@ -0,0 +1,61 @@
+angular.module('comment', [])
+	.controller('MainCtrl', [
+  	'$scope','$http',
+  function($scope,$http){
+ $scope.test = 'Hello world!';
+
+$scope.comments = [
+      {title:'Comment 1', upvotes:5},
+      {title:'Comment 2', upvotes:6},
+      {title:'Comment 3', upvotes:1},
+      {title:'Comment 4', upvotes:4},
+      {title:'Comment 5', upvotes:3}
+    ];
+
+ $scope.addComment = function() {
+if($scope.formContent === '') { return; }
+      console.log("In addComment with "+$scope.formContent);
+      $scope.create({
+        title: $scope.formContent,
+        upvotes: 0,
+      });
+      $scope.formContent = '';     
+ $scope.formContent='';
+    };
+
+$scope.incrementUpvotes = function(comment) {
+      $scope.upvote(comment);
+    };
+
+$scope.getAll = function() {
+    return $http.get('/comments').success(function(data){
+      angular.copy(data, $scope.comments);
+    });
+  };
+
+$scope.create = function(comment) {
+    return $http.post('/comments', comment).success(function(data){
+      $scope.comments.push(data);
+    });
+  };
+
+$scope.upvote = function(comment) {
+      return $http.put('/comments/' + comment._id + '/upvote')
+        .success(function(data){
+          console.log("upvote worked");
+          comment.upvotes += 1;
+        });
+    };
+
+$scope.getAll();
+  
+$scope.images = [
+		{"thumbnail":"../images/nerd.jpg", "description":"nerdy emoji"},
+		{"thumbnail":"../images/eyeglasses.png", "description":"eyeglasses"},
+		{"thumbnail":"../images/wink.png", "description":"wink"},
+		{"thumbnail":"../images/smiley.png", "description":"smiley"},
+	];
+
+}
+
+]);
